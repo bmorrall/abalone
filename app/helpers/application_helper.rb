8afaec9d01520c955a625fbc9cc9d8cddf23542d
Ben Morrall <bemo56@hotmail.com>
2024-09-20 12:33:05 +0800

    Add cancel and back span helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  # <span>&lt;&lt; Back</span>
+  def back_span_tag
+    content_tag(:span, "<< Back")
+  end
+
+  # <span>&lt;&lt; Cancel</span>
+  def cancel_span_tag
+    content_tag(:span, "<< Cancel")
+  end
 end
